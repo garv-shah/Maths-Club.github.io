980916f5bafc44a42bfc46e7644fb1ac298aecae
anonymous3141 <junhua.chen.ios@gmail.com>
2021-06-03 11:46:42 +1000

    make loadFeed procedral

--- loadFeed.js
+++ loadFeed.js
@@ -1,6 +1,4 @@
-// load atom feed to body
-    function fetchPosts() {
-      var request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
       const ATOM_FEED_URL = "https://maths-club.github.io/feed.xml";
       const ERROR_MSG = "Uh Oh: Cannot load posts at the moment :("
       request.open("GET", ATOM_FEED_URL, true);
@@ -9,15 +7,7 @@
       request.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
            console.log(this.responseText)
-           loadPosts(this.responseXML);
-         } else if (this.readyState == 4) {
-           document.getElementById("feed").innerHTML = ERROR_MSG;
-         }
-       };
-    }
-   
-    function loadPosts(feedXml) {
-      console.log(feedXml.getElementsByTagName("feed")[0])
+           console.log(feedXml.getElementsByTagName("feed")[0])
        var listPosts = feedXml.getElementsByTagName("feed")[0].getElementsByTagName("entry");
        console.log(listPosts.length)
        //posts are ordered from most recent to least
@@ -37,5 +27,7 @@
          postElem.innerHTML = "<h3>" + title + "<h3><h4> Date: " + published + "</h4><p>"+excerpt + "</p><br><br><a href='"+link+"'>READ MORE </a>"
          document.querySelector("body").appendChild(postElem)
        }
-    }
-    fetchPosts()
+         } else if (this.readyState == 4) {
+           alert(ERROR_MSG);
+         }
+       };
