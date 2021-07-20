16ac808e6718605ec5bef54f090c809a9a359d00
Garv <gshah.6110@gmail.com>
2021-07-20 22:51:52 +1000

    Added comments to loadFeed.js and refactored some code
    There were a typos in loadFeed.js, and the text interpolation with string literals looks much nicer when loading the inner HTML for the postElem

--- loadFeed.js
+++ loadFeed.js
@@ -1,5 +1,6 @@
-// load atom feed to body
+// this script loads atom/rss feed to the "block-content" div
 
+//creates an empty textbox which inherits the values of clean-blog-post
 function makeBlogPost() {
   var postElem = document.createElement("div")
   var CSSattr = document.createAttribute("class")
@@ -7,6 +8,8 @@ function makeBlogPost() {
   postElem.setAttributeNode(CSSattr)
   return postElem;
 }
+
+//fetches the posts from the rss feed
 function fetchPosts() {
   var request = new XMLHttpRequest();
   const ATOM_FEED_URL = "https://maths-club.github.io/feed.xml";
@@ -16,34 +19,45 @@ function fetchPosts() {
   request.send();
   request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      //console.log(this.responseText)
       loadPosts(this.responseXML);
     } else if (this.readyState == 4) {
-      var txtbox = makeBlogPost();
-      txtbox.innerHTML = ERROR_MSG;
-      document.querySelector("#page > main > section > div > div.block-content").appendChild(postElem)
+      let textbox = makeBlogPost();
+      textbox.innerHTML = ERROR_MSG;
+      document.querySelector("#page > main > section > div > div.block-content").appendChild(textbox)
     }
   };
 }
 
 
 function loadPosts(feedXml) {
-//console.log(feedXml.getElementsByTagName("feed")[0])
- var listPosts = feedXml.getElementsByTagName("feed")[0].getElementsByTagName("entry");
-// console.log(listPosts.length)
- //posts are ordered from most recent to least
- for (var i=0; i<listPosts.length; i++) {
-   post = listPosts.item(i); //get ith post
-   var link = post.getElementsByTagName("link")[0].attributes.getNamedItem("href").value;
-   //mother of god why
-   var excerpt = post.getElementsByTagName("summary")[0].textContent;
-   var title = post.getElementsByTagName("title")[0].textContent;
-   var published = post.getElementsByTagName("published")[0].textContent;
-   published = published.substring(0, 10).replaceAll("-","/") //clean to YYYY-MM-DD
-   //console.log(excerpt, title, published, link)
-   var postElem = makeBlogPost();
-   postElem.innerHTML = "<h3>" + title + '</h3><div class=\"info\"><span style="color: var(--muted-text-color)">' + published + "</span></div><p>"+excerpt +'</p><a class="btn btn-outline-primary btn-sm" role="button" href="'+link+'" style="border-color: var(--highlight-color);color: var(--highlight-color);">Read More</a>'
-   document.querySelector("#page > main > section > div > div.block-content").appendChild(postElem)
- }
+  const listPosts = feedXml.getElementsByTagName("feed")[0].getElementsByTagName("entry");
+
+  for (let i=0; i<listPosts.length; i++) {
+    let post = listPosts.item(i); //gets the post from the list
+
+    //extracts the information needed from the post
+    const link = post.getElementsByTagName("link")[0].attributes.getNamedItem("href").value;
+    const excerpt = post.getElementsByTagName("summary")[0].textContent;
+    const title = post.getElementsByTagName("title")[0].textContent;
+    let published = post.getElementsByTagName("published")[0].textContent;
+    published = published.substring(0, 10).replaceAll("-","/") //cleans to YYYY-MM-DD
+
+    let postElem = makeBlogPost(); //creates an empty textbox from the function above
+
+    //this code just creates the html for our textbox using the variables defined above, and then pushes it the inner HTML of the post element
+    postElem.innerHTML = `
+    <h3>${title}</h3>
+    <div class=\"info\">
+        <span style="color: var(--muted-text-color)">${published}</span>
+    </div>
+    <p>${excerpt}</p>
+    <a class="btn btn-outline-primary btn-sm" role="button" href="${link}" style="border-color: var(--highlight-color); color: var(--highlight-color);">
+        Read More
+    </a>
+    `
+
+    //finally, here we push the final post element into the block-content div
+    document.querySelector("#page > main > section > div > div.block-content").appendChild(postElem)
+  }
 }
 fetchPosts()
